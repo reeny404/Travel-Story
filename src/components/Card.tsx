d9a45a86ac18da6d9ef046bb0b41d66a55b5ad32
Joseph Park <rkfnahs12e@gmail.com>
2024-07-23 13:10:08 +0900

    feat : Card component 분리 중 -> imageContainer 생성

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,16 +1,15 @@
-import Image from "next/image";
+import ImageContainer from "./imageContainer";
+
+//image 컨테이너 , text container, areaForm (intro, detail, recommend),
 
 function Card() {
   return (
     <div className="flex flex-col justify-center items-center">
-      <div className="relative w-full h-[353px] bg-[#f0f0f0]">
-        <Image
-          src="https://yqoupynehwgshtspamuf.supabase.co/storage/v1/object/public/country/Italy.jpg"
-          alt="이미지"
-          layout="fill"
-          objectFit="cover"
-        />
-      </div>
+      <ImageContainer
+        isTitle
+        size="small"
+        imageUrl="https://yqoupynehwgshtspamuf.supabase.co/storage/v1/object/public/country/Italy.jpg"
+      />
       <div className="p-4 m-2 w-full">
         <h2 className="text-xl font-bold">
           르네상스의 발상지, 예술과 낭만의 중심 이탈리아
@@ -21,17 +20,12 @@ function Card() {
         </p>
       </div>
       <hr />
-      <div className="relative w-full h-[562px] bg-[#f0f0f0] aspect-auto 0">
-        <Image
-          src="https://yqoupynehwgshtspamuf.supabase.co/storage/v1/object/public/country/Italy.jpg"
-          alt="이미지"
-          fill
-          objectFit="cover"
-        />
-        <h1 className="absolute bottom-0 left-0 -mb-6 text-[64px] font-bold ">
-          ITALY
-        </h1>
-      </div>
+      <ImageContainer
+        isTitle={true}
+        title="ITALY"
+        size="small"
+        imageUrl="https://yqoupynehwgshtspamuf.supabase.co/storage/v1/object/public/country/Italy.jpg"
+      />
       <div className="p-9 pt-7 pb-16 m-2 w-full h-[250px] flex flex-col justify-around">
         <h2 className="font-semibold">로마</h2>
         <h2 className="font-semibold">나폴리</h2>
---
+++ src/components/imageContainer.tsx
@@ -0,0 +1,42 @@
+import { VariantProps, cva } from "class-variance-authority";
+import Image from "next/image";
+
+type imageContainerProps = imageVariantProps & {
+  title?: string;
+  isTitle: boolean;
+  imageUrl: string;
+};
+
+type imageVariantProps = VariantProps<typeof imageVariant>;
+
+const imageVariant = cva("relative w-full bg-[#f0f0f0] aspect-auto", {
+  variants: {
+    size: {
+      large: "h-[562px]",
+      small: "h-[353px]",
+    },
+  },
+  defaultVariants: {
+    size: "large",
+  },
+});
+
+const ImageContainer = ({
+  size,
+  isTitle,
+  title,
+  imageUrl,
+}: imageContainerProps) => {
+  return (
+    <div className={imageVariant({ size })}>
+      <Image src={imageUrl} alt="이미지" layout="fill" objectFit="cover" />
+      {isTitle && (
+        <h1 className="absolute bottom-0 left-0 -mb-6 text-[64px] font-bold">
+          {title}
+        </h1>
+      )}
+    </div>
+  );
+};
+
+export default ImageContainer;
