57532a22ab59c25d2677803972614e6a989cd889
Joseph Park <rkfnahs12e@gmail.com>
2024-07-23 12:32:21 +0900

    feat : Card intro 페이지 형식 구현

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -20,6 +20,26 @@ function Card() {
           소개여행지 소개여행지 소개여행지 소개
         </p>
       </div>
+      <hr />
+      <div className="relative w-full h-[562px] bg-[#f0f0f0] aspect-auto 0">
+        <Image
+          src="https://yqoupynehwgshtspamuf.supabase.co/storage/v1/object/public/country/Italy.jpg"
+          alt="이미지"
+          fill
+          objectFit="cover"
+        />
+        <h1 className="absolute bottom-0 left-0 -mb-6 text-[64px] font-bold ">
+          ITALY
+        </h1>
+      </div>
+      <div className="p-9 pt-7 pb-16 m-2 w-full h-[250px] flex flex-col justify-around">
+        <h2 className="font-semibold">로마</h2>
+        <h2 className="font-semibold">나폴리</h2>
+        <h2 className="font-semibold">피렌체</h2>
+        <h2 className="font-semibold">볼로냐</h2>
+        <h2 className="font-semibold">베네치아</h2>
+      </div>
+      <div className="w-[134px] h-1 bg-black opacity-70 fixed bottom-3"></div>
     </div>
   );
 }
