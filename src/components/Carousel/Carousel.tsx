95563a2ff689eb9d35bdfd5b5ff88ec3a597a70d
Joseph Park <rkfnahs12e@gmail.com>
2024-07-24 14:34:27 +0900

    feat : #11 Carousel 컴포넌트 추가 css작업 x

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,39 +1,22 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import ImageContainer from "../Card/ImageContainer";
+import { EmblaOptionsType } from "embla-carousel";
+import Autoplay from "embla-carousel-autoplay";
+import useEmblaCarousel from "embla-carousel-react";
+import { ReactNode } from "react";
 
-function Carousel() {
-  // 이 값이 바뀌면 다음 페이지로
-  const [page, setPage] = useState<number>(1);
+type CarouselProps = {
+  slides: ReactNode[];
+  options?: EmblaOptionsType;
+};
 
-  useEffect(() => {
-    let int = setInterval(() => {
-      setPage((prev) => (prev + 1 >= 8 ? 1 : prev + 1));
-    }, 5000);
-
-    return () => clearInterval(int);
-  }, []);
+function Carousel({ slides, options }: CarouselProps) {
+  const [emblaRef] = useEmblaCarousel(options, [Autoplay({ delay: 3000 })]);
 
   return (
-    <main className="relative flex h-full w-full flex-col justify-center items-center">
-      <div className=" absolute w-full h-full">
-        <ImageContainer
-          isTitle
-          size="detail"
-          imageUrl="https://yqoupynehwgshtspamuf.supabase.co/storage/v1/object/public/country/Italy.jpg"
-        />
-        <div className="absolute bottom-8 left-10 py-3 px-6 bg-[#0000007c] rounded-lg">
-          <h2 className="text-4xl">Beautiful landscape{page}</h2>
-          <p className="text-2xl mt-4 text-purple-200">The magic of nature!</p>
-        </div>
-      </div>
-
-      {/* left arrow -> 클릭 시 page num -1 등등 */}
-      <div className="z-10 fixed bottom-1/2 left-4 text-2xl">
-        <span>&lt;-</span>
-      </div>
-    </main>
+    <div className="embla" ref={emblaRef}>
+      <div className="embla__container flex">{slides}</div>
+    </div>
   );
 }
 
---
+++ src/components/Carousel/CarouselWrapper.tsx
@@ -0,0 +1,18 @@
+import { EmblaOptionsType } from "embla-carousel";
+import { ReactNode } from "react";
+import Carousel from "./Carousel";
+type CarouselItem = { items: ReactNode[] };
+
+// 이곳에 JSX로 이루어진 데이터 베열을 넣는다면 이를 케러셀로 리턴해줍니다.
+
+const CarouselWrapper = ({ items }: CarouselItem) => {
+  const OPTIONS: EmblaOptionsType = { align: "center", loop: true };
+
+  return (
+    <section>
+      <Carousel slides={items} options={OPTIONS} />
+    </section>
+  );
+};
+
+export default CarouselWrapper;
