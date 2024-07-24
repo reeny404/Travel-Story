af1fbc6d24473efb688442db9e9cde23713b0553
Joseph Park <rkfnahs12e@gmail.com>
2024-07-24 14:43:30 +0900

    fix : #2 country/route.ts 파일의 리턴값 NextResponse로 감싸서 리턴하여 vercel 에러 수정

--- src/app/api/country/route.ts
+++ src/app/api/country/route.ts
@@ -1,5 +1,5 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-  return null;
+  return NextResponse.json(null);
 }
