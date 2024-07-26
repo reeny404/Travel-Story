cfc09ccf4c3dc6802baf0fecc5991ae72f4a69e9
Joseph Park <rkfnahs12e@gmail.com>
2024-07-26 17:08:37 +0900

    update : #24 zustand 코드에 persist 타입 추가

--- src/stores/recommend.store.ts
+++ src/stores/recommend.store.ts
@@ -1,15 +1,31 @@
 import { create } from "zustand";
+import { persist, PersistOptions } from "zustand/middleware";
 
 type RecommendStore = {
   cityId: number;
   setCityId: (cityId: number) => void;
   countryId: number;
+  setCountryId: (countryId: number) => void;
 };
 
-const useRecommendStore = create<RecommendStore>((set) => ({
-  cityId: 1,
-  countryId: 1,
-  setCityId: (cityId: number) => set((state) => ({ cityId })),
-}));
+type MyPersist = (
+  config: (set: any, get: any, api: any) => RecommendStore,
+  options: PersistOptions<RecommendStore>
+) => (set: any, get: any, api: any) => RecommendStore;
+
+const useRecommendStore = create<RecommendStore>(
+  (persist as MyPersist)(
+    (set) => ({
+      cityId: 1,
+      countryId: 1,
+      setCityId: (cityId: number) => set({ cityId }),
+      setCountryId: (countryId: number) => set({ countryId }),
+    }),
+    {
+      name: "recommend-storage",
+      getStorage: () => localStorage,
+    }
+  )
+);
 
 export default useRecommendStore;
