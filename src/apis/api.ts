e1dcee687bf2dd3fc28478990e3e9d3e56b92f0b
reeny404 <happy2hyeon@kakao.com>
2024-07-26 17:07:06 +0900

    update: api.ts 로 한번 래핑하도록 변경 #10

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosInstance } from "axios";
+import { AccountAPI } from "./account.api";
 import AreaAPI from "./area.api";
 import CityAPI from "./city.api";
 import CountryAPI from "./country.api";
@@ -9,12 +10,15 @@ class API {
   country;
   city;
   area;
+  account;
 
   constructor() {
+    // TODO base URL BASE_URL 상수로 교체 필요 -> .env.local 에 값 추가 요청 해야함 
     this.axios = axios.create({ baseURL: "http://localhost:3000/" });
     this.country = new CountryAPI(this.axios);
     this.city = new CityAPI(this.axios);
     this.area = new AreaAPI(this.axios);
+    this.account = new AccountAPI(this.axios);
   }
 }
 
