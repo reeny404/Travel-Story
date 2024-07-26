c9b23c548d7ec433b9495e61680c1585a104268d
Joseph Park <rkfnahs12e@gmail.com>
2024-07-26 18:20:55 +0900

    chore : #24 areaDetailPage -> areaTypePage로 수정 후 경로 city 아래로 이동

---
+++ src/app/(provider)/(root)/recommend/city/[id]/[areaType]/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { api } from "@/apis/api";
+import useRecommendStore from "@/stores/recommend.store";
+import { filterByAreaType } from "@/utils/filterByAreaType";
+import { useQuery } from "@tanstack/react-query";
+import { usePathname } from "next/navigation";
+import AreaCard from "../../../_components/AreaCard";
+
+function AreaTypePage() {
+  const pathname = usePathname();
+  const areaType = pathname.split("/").slice(-1)[0];
+  const { cityId } = useRecommendStore();
+
+  const { data: areas } = useQuery({
+    queryKey: ["area", cityId],
+    queryFn: () => api.area.getAreasByCity(cityId),
+    select: (data) => data?.data,
+  });
+
+  const filteredArea = filterByAreaType(areas!, areaType);
+
+  return (
+    <div className="container overflow-x-hidden w-screen h-screen max-w-[375px] mx-auto flex-col">
+      {filteredArea?.map((area, idx) => {
+        return (
+          <AreaCard
+            key={idx}
+            title={area.title}
+            description={area.description}
+            rating={4}
+            imageUrl={area.imageUrl!}
+            linkUrl="/"
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default AreaTypePage;
