c25e5000c3dd1850c938bd08a43ec56fc71aad01
Leejunhyeok03 <ly0609@naver.com>
2024-07-29 01:22:09 +0900

    feat:time BottomSheetInput 추가

--- src/app/(provider)/(root)/plan/_components/BottomSheet.tsx
+++ src/app/(provider)/(root)/plan/_components/BottomSheet.tsx
@@ -6,12 +6,25 @@ import BottomSheetTitle from "./BottomSheetTitle";
 function BottomSheet({ type, status }: BottomSheetType) {
   return (
     <div className="fixed top-0 left-0 w-full h-full">
-      <div className="absolute bottom-0 left-0 w-full h-auto py-4 pb-8 px-4 flex flex-col gap-3 rounded-t-lg shadow-bottom-sheet">
+      <form className="absolute bottom-0 left-0 w-full h-auto py-4 pb-8 px-4 flex flex-col gap-3 rounded-t-lg shadow-bottom-sheet">
         <BottomSheetTitle type={type} status={status} />
-        <BottomSheetInput type="memo" />
-        <BottomSheetInput type="spend" />
-        <BottomSheetInput type="place" />
-      </div>
+        <BottomSheetInput
+          type="time"
+          isDisabled={status === "read" ? true : false}
+        />
+        <BottomSheetInput
+          type="memo"
+          isDisabled={status === "read" ? true : false}
+        />
+        <BottomSheetInput
+          type="spend"
+          isDisabled={status === "read" ? true : false}
+        />
+        <BottomSheetInput
+          type="place"
+          isDisabled={status === "read" ? true : false}
+        />
+      </form>
     </div>
   );
 }
--- src/app/(provider)/(root)/plan/_components/BottomSheetInput.tsx
+++ src/app/(provider)/(root)/plan/_components/BottomSheetInput.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 type BottomSheetInputType = {
   isDisabled?: boolean;
   value?: string;
-  type: "memo" | "spend" | "place";
+  type: "memo" | "spend" | "place" | "time";
 };
 
 export default function BottomSheetInput({
@@ -31,6 +31,10 @@ export default function BottomSheetInput({
         setPlaceholder("위치 추가하기");
         setIcon("📍"); // 예시 위치 아이콘
         break;
+      case "time":
+        setPlaceholder("시간 선택");
+        setIcon("⏰"); // 예시 시간 아이콘
+        break;
       default:
         setPlaceholder("");
         setIcon("❓"); // 예시 기본 아이콘
@@ -41,11 +45,74 @@ export default function BottomSheetInput({
     setInputValue(event.target.value);
   };
 
+  const handleStartTimeChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const newStartTime = event.target.value;
+    if (newStartTime > endTime && endTime !== "") {
+      alert("시작 시간은 종료 시간보다 늦을 수 없습니다.");
+      return;
+    }
+    setStartTime(newStartTime);
+  };
+
+  const handleEndTimeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newEndTime = event.target.value;
+    if (newEndTime < startTime && startTime !== "") {
+      alert("종료 시간은 시작 시간보다 이를 수 없습니다.");
+      return;
+    }
+    setEndTime(newEndTime);
+  };
+
+  const calculateDuration = () => {
+    if (startTime && endTime) {
+      const start = new Date(`2024-01-01T${startTime}:00`);
+      const end = new Date(`2024-01-01T${endTime}:00`);
+      const duration = (end.getTime() - start.getTime()) / 60000; // 분 단위로 계산
+      return `${duration} 분`;
+    }
+    return "";
+  };
+
+  const [startTime, setStartTime] = useState("");
+  const [endTime, setEndTime] = useState("");
+
+  if (type === "time") {
+    return (
+      <div className="flex items-center">
+        <i className="mr-2 w-8 text-center">{icon}</i>
+        <input
+          className="outline-0 w-22 border-[1px] text-sm border-gray appearance-none"
+          type="time"
+          name={type}
+          value={startTime}
+          disabled={isDisabled}
+          onChange={handleStartTimeChange}
+          placeholder={placeholder}
+        />
+        <p className="m-3">-</p>
+        <input
+          className="outline-0 w-22 border-[1px] text-center text-sm border-gray appearance-none"
+          type="time"
+          name={type}
+          value={endTime}
+          disabled={isDisabled}
+          onChange={handleEndTimeChange}
+          placeholder={placeholder}
+        />
+        <p className="pl-3 ml-2 border-l-[1px] border-gray-300">
+          {calculateDuration()}
+        </p>
+      </div>
+    );
+  }
+
   return (
     <div className="flex items-center">
       <i className="mr-2 w-8 text-center">{icon}</i>
       <input
-        className="border-0 outline-0 w-[90%] border-b-[1px] text-sm border-white transition-colors focus:border-blue-300"
+        className="border-0 outline-0 w-[90%] border-b-[1px] text-sm border-white"
         type={type === "spend" ? "number" : "text"}
         name={type}
         value={inputValue}
