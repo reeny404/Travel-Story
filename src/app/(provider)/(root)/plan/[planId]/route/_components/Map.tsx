37ae6b872fd4940a6cb21a220d10d6759b31450f
reeny404 <happy2hyeon@kakao.com>
2024-07-30 23:11:15 +0900

    chore: [plan] 여행 경로 보여주기 #27 - 테스트 단계, 고정 데이터로 경로 찍기

---
+++ src/app/(provider)/(root)/plan/[planId]/route/_components/Map.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { LatLng } from "@/types/LatLng";
+import {
+  GoogleMap,
+  LoadScript,
+  Marker,
+  Polyline,
+} from "@react-google-maps/api";
+import React, { useState } from "react";
+
+type props = { locations: LatLng[] };
+
+function Map({ locations }: props) {
+  const [isInit, setIsInit] = useState<boolean>(false);
+  const center: LatLng = getCenter(locations);
+
+  const handleOnLoad = () => setIsInit(true);
+
+  return (
+    <LoadScript
+      googleMapsApiKey="AIzaSyDl9L5mIy1L4UfpIWPJ_2yUJznFw02lCNQ"
+      onLoad={handleOnLoad}
+    >
+      <GoogleMap
+        mapContainerStyle={{
+          width: "100%",
+          height: "100vh",
+        }}
+        center={center}
+        zoom={7}
+      >
+        {locations.map((location, i) =>
+          isInit ? (
+            <Marker
+              key={i}
+              position={location}
+              label={i + ""}
+              icon={{
+                path: google.maps.SymbolPath.CIRCLE,
+                fillColor: "#e08181",
+                fillOpacity: 1,
+                scale: 10,
+                strokeColor: "black",
+                strokeWeight: 2,
+              }}
+            />
+          ) : (
+            <Marker key={i} position={location} label={i + ""}></Marker>
+          )
+        )}
+        <Polyline
+          path={locations}
+          options={{
+            strokeColor: "#535353",
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+          }}
+        />
+      </GoogleMap>
+    </LoadScript>
+  );
+}
+
+export default React.memo(Map);
+
+function getCenter(locations: LatLng[]): LatLng {
+  let lat = 0,
+    lng = 0;
+  const length = locations.length;
+  for (let i = 0; i < length; i++) {
+    const item = locations[i];
+    lat += item.lat;
+    lng += item.lng;
+  }
+
+  return { lat: lat / length, lng: lng / length };
+}
