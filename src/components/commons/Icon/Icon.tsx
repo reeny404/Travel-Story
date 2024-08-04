42e8b27198c85dab2677b372a5bf44ad80b08766
reeny404 <happy2hyeon@kakao.com>
2024-08-04 11:21:31 +0900

    chore: 개발자도구 waring 제거 - warning 메세지 : Image with has either width or height modified, but not the other. If you use CSS to change the size of your image, also include the styles 'width: "auto"' or 'height: "auto"' to maintain the aspect ratio.

--- src/components/commons/Icon/Icon.tsx
+++ src/components/commons/Icon/Icon.tsx
@@ -17,7 +17,13 @@ function Icon({ icon, alt = "icon", size = 20, onClick }: IconProps) {
       className="flex justify-center items-center w-11 h-11"
       onClick={onClick}
     >
-      <Image src={iconPath} alt={alt} width={size} height={size} />
+      <Image
+        src={iconPath}
+        alt={alt}
+        width={size}
+        height={size}
+        className="w-auto h-auto"
+      />
     </button>
   );
 }
