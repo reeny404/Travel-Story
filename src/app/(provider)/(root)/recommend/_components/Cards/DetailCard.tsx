89372526ae98bf1e46edea94678052c4ce5734ac
reeny404 <happy2hyeon@kakao.com>
2024-08-04 11:30:53 +0900

    chore: solve console error

--- src/app/(provider)/(root)/recommend/_components/Cards/DetailCard.tsx
+++ src/app/(provider)/(root)/recommend/_components/Cards/DetailCard.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import ImageFrame from "@/components/Frame/ImageFrame";
 
 type DetailCardProps = {
   title: string;
@@ -9,7 +9,11 @@ type DetailCardProps = {
 function DetailCard({ title, description, imageUrl }: DetailCardProps) {
   return (
     <div className="w-full relative aspect-4/5">
-      <Image src={imageUrl || "/"} alt={title} fill className="object-cover" />
+      <ImageFrame
+        src={imageUrl}
+        alt={title}
+        className="w-full min-h-[538px] object-cover"
+      />
 
       {/* <div className="p-4">
         <p className="text-xl font-bold mt-2 mb-2">{title}</p>
