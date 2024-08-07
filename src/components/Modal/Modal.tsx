7df0b06e24d1cdca577c3e8aa095518db075af7e
Joseph Park <rkfnahs12e@gmail.com>
2024-08-07 11:14:39 +0900

    style : #179 #180 로그인 검사 모달 디자인, 버그 수정 및 상세 페이지 상단 카드 아치형 그림자 추가

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -11,22 +11,26 @@ function Modal() {
   }
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50">
+    <div className="fixed inset-0 flex items-center justify-center z-50 px-5">
       <div className="fixed inset-0 bg-black opacity-50"></div>
 
-      <article className="relative bg-white p-6 border border-gray-300 rounded-lg shadow-lg max-w-md w-full z-10">
-        <h1 className="text-2xl font-bold mb-4 text-center">{title}</h1>
-        <p className="text-gray-700 mb-6 text-center">{content}</p>
+      <article className="relative bg-white px-7 py-8 border border-gray-300 rounded-lg shadow-lg max-w-md w-full z-10">
+        {/* <h1 className="text-2xl font-bold mb-4 text-center">{title}</h1> */}
+        <p className="text-neutral-750 mb-10 text-center text-lg px-10">
+          {content}
+        </p>
         <div className="flex justify-between">
           <button
             onClick={closeModal}
-            className="w-1/2 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded mr-2"
+            className="w-1/2 h-10 text-gray-800 py-2 border-[0.6px] border-neutral-600 rounded mr-2"
           >
-            닫기
+            취소
           </button>
           <button
-            onClick={() => router.push("/login")}
-            className="w-1/2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => {
+              closeModal(), router.push("/login");
+            }}
+            className="w-1/2 h-10 bg-neutral-750 text-white py-2 rounded"
           >
             로그인하기
           </button>
