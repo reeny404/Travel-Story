20b70061396e8e6d83dae13e777226fb3dcc24fb
hyeonseok98 <157561573+hyeonseok98@users.noreply.github.com>
2024-08-13 13:00:56 +0900

    fix: build 에러 해결

--- src/app/(provider)/(root)/search/page.tsx
+++ src/app/(provider)/(root)/search/page.tsx
@@ -1,4 +1,5 @@
 import MainLayout from "@/components/Layout/MainLayout";
+import { Suspense } from "react";
 import ClientSearch from "./_components/ClientSearch"; // 일반적으로 임포트
 
 function SearchPage() {
@@ -11,7 +12,9 @@ function SearchPage() {
     >
       <section className="min-h-[calc(100vh-52px)]">
         <div className="flex flex-col">
-          <ClientSearch />
+          <Suspense fallback={<div />}>
+            <ClientSearch />
+          </Suspense>
         </div>
       </section>
     </MainLayout>
