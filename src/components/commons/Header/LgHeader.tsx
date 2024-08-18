483df28a6663dafa655385041ba1427d9aa09df6
parkchowon <70216263+parkchowon@users.noreply.github.com>
2024-08-18 17:46:24 +0900

    update: #270 헤더 클릭 시 이동

--- src/components/commons/Header/LgHeader.tsx
+++ src/components/commons/Header/LgHeader.tsx
@@ -1,22 +1,75 @@
+"use client";
+
+import { api } from "@/apis/api";
 import SearchBar from "@/components/SearchBar/SearchBar";
 import { dmSerifDisplayFont } from "@/constants/fonts";
+import { useAuth } from "@/contexts/auth.contexts";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import SvgIcon from "../SvgIcon";
 
 function LgHeader() {
+  const { isInitialized, isLoggedIn, setUser } = useAuth();
+  const [isPlanBold, setIsPlanBold] = useState(false);
+  const [isAreaBold, setIsAreaBold] = useState(false);
+
+  const router = useRouter();
+  const path = usePathname();
+
+  useEffect(() => {
+    const recommend = path.includes("recommend") || path === "/";
+    const myPlan = path.includes("plan");
+
+    setIsAreaBold(recommend);
+    setIsPlanBold(myPlan);
+  }, [path]);
+
+  const handleIsLoginUser = (value: string) => {
+    if (isInitialized && isLoggedIn) {
+      return router.push(`/${value}`);
+    } else {
+      return router.push("/login");
+    }
+  };
+
+  const handleLogout = async () => {
+    setUser(null);
+    await api.auth.logout();
+  };
+
+  const handleLogin = () => {
+    router.push("/login");
+  };
+
+  const handleHomeClick = () => {
+    if (!isAreaBold) {
+      router.push("/");
+    }
+  };
+
   return (
-    <header className="w-full h-[116px] items-center px-8 hidden sm:block">
+    <header className="w-full items-center px-8 hidden sm:block">
       <div className="flex flex-row items-center h-8 text-[12px] space-x-6">
         <div className="flex-grow" />
-        <p>마이페이지</p>
-        <p>보관함</p>
-        <p>로그인</p>
+        <button onClick={() => handleIsLoginUser("my")}>마이페이지</button>
+        <button onClick={() => handleIsLoginUser("my/bookmarks")}>
+          보관함
+        </button>
+        {isInitialized && isLoggedIn ? (
+          <button onClick={handleLogout}>로그아웃</button>
+        ) : (
+          <button onClick={handleLogin}>로그인</button>
+        )}
       </div>
-      <div className="flex flex-row w-full items-center justify-between">
-        <h1
-          className={`${dmSerifDisplayFont.className} text-[28px] leading-[38px] whitespace-nowrap`}
-        >
-          Travel Story
-        </h1>
+      <div className="flex flex-row w-full pt-3 pb-6 items-center justify-between">
+        <Link href={"/"}>
+          <h1
+            className={`${dmSerifDisplayFont.className} text-[28px] leading-[38px] whitespace-nowrap`}
+          >
+            Travel Story
+          </h1>
+        </Link>
         <div className="flex flex-row w-[600px] space-x-2">
           <SearchBar />
           <button
@@ -26,10 +79,20 @@ function LgHeader() {
             <SvgIcon name="slider" width={18} height={18} title="filter" />
           </button>
         </div>
-        <div className="flex flex-row items-center text-xl py-3 w-fit">
-          <p>여행지</p>
+        <div className="flex flex-row items-center text-xl py-3 w-fit font-light">
+          <button
+            onClick={handleHomeClick}
+            className={`${isAreaBold ? "font-medium" : ""}`}
+          >
+            여행지
+          </button>
           <div className="mx-4 w-[1px] h-4 bg-neutral-300" />
-          <p>내 여행 플래너</p>
+          <button
+            onClick={() => handleIsLoginUser("plan/recent")}
+            className={`${isPlanBold ? "font-medium" : ""}`}
+          >
+            내 여행 플래너
+          </button>
         </div>
       </div>
     </header>
